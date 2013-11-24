d6783fc048def295cd1616192f1bc4c38bf712ec
SoonJae Kwon <kwonsjay@gmail.com>
2013-11-23 17:41:38 -0800

    change root to session new

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Textroulette::Application.routes.draw do
   resources :users, :only => [:create, :new, :show]
   resource :session, :only => [:create, :destroy, :new]
 
-  root :to => "users#show"
+  root :to => "sessions#new"
 end
