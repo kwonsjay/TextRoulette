71a8df1222bfc1a7fc27adfcd2404de33c6bb792
Irene Nguyen <irene.m.nguyen@gmail.com>
2013-11-23 16:00:49 -0800

    initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Textroulette::Application.config.session_store :cookie_store, key: '_textroulette_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Textroulette::Application.config.session_store :active_record_store
